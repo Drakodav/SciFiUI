67305f2c36eb56c219379fd43cfa86cd8efe27c6
Vlad Medves <vmedves@gmail.com>
2019-03-21 12:54:25 +0000

    .

--- java/src/ie/tudublin/Stars.java
+++ java/src/ie/tudublin/Stars.java
@@ -6,12 +6,11 @@ public class Stars
     private float unit;
     private float x;
     private float y;
-    private float jitter;
 
     public Stars(UI ui, float unit)
     {
         this.ui = ui;
-        this.unit = unit/24; // 1 size unit
+        this.unit = unit/64; // 1 size unit
 
         // set star location to spawn in a random location
         x = (float) (Math.random() * (ui.width - 0) + 1);
@@ -30,23 +29,21 @@ public class Stars
 
         ui.pushMatrix();
         ui.translate(x, y);
-        if (x>ui.width/2 && y<ui.height/2) {
-            ui.rotate((float)Math.cos(jitter));
-        }else{
-            ui.rotate((float)Math.sin(jitter));
-        }
 
-        ui.line(0, 0, +unit , 0); // horizontal
-        ui.line(+unit/2, -unit/2, +unit/2, +unit/2); // vertical
-        ui.line(+unit/6, -unit/3, +unit-unit/6, +unit/3); // slash downwards
-        ui.line(+unit/6, +unit/3, +unit-unit/6, -unit/3); // slash upwards
+        ui.line(-unit, 0, unit, 0); // horizontal
+        ui.line(0, unit, 0, -unit); // vertical
+        ui.line(-unit/6*4, unit/6*4, unit/6*4, -unit/6*4 ); // slash downwards
+        ui.line(-unit/6*4, -unit/6*4, unit/6*4, unit/6*4 ); // slash upwards
         
         ui.popMatrix();
     }
 
     public void update()
     {
-        jitter += 0.02f;
+        x = (float) (x + (Math.random() * (1) + 1));
+        x = (float) (x - (Math.random() * (1) + 1));
+        y = (float) (y + (Math.random() * (1) + 1));
+        y = (float) (y - (Math.random() * (1) + 1));
     }
 
 }
